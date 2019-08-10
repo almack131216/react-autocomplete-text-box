51d878066f9a25a3a7ecc8b5ac570f56a9261b4b
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-08-10 18:37:44 +0700

    WORKING | autocomplete added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HideableText from './HideableText';
+import AutoCompleteText from './AutoCompleteText';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-content">
-          <HideableText text="Dynamic Text!"/>
-        </p>        
+        <AutoCompleteText />
+        <HideableText text="Dynamic Text!"/>          
       </div>
     )
   }
